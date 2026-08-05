510ab1afc6e204788689baf5389ccc82506652c8
William Conner <redwils5@gmail.com>
2026-08-05 13:08:12 -0400

    Simple single threaded simulation with text output

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,62 @@
+#include <mdspan>
 #include <print>
+#include <ranges>
+#include <vector>
 
+constexpr auto DOMAIN_WIDTH{32u};
+constexpr auto DOMAIN_HEIGHT{32u};
+constexpr auto EPSILON{1.f};
+constexpr auto MAX_ITERATIONS{100'000u};
+
+constexpr auto HOT{100.f};
+constexpr auto COLD{0.f};
 int main(int argc, char *argv[]) {
-    std::println("Hello!");
+    // Init data
+    std::vector<float> domain_curr(DOMAIN_WIDTH * DOMAIN_HEIGHT, COLD);
+    std::vector<float> domain_next(DOMAIN_WIDTH * DOMAIN_HEIGHT);
+    auto norm_diff{0.f};
+
+    // Set boundary conditions
+    auto set_boundary_conditions{[&](auto &domain) {
+        /// North & South: Hot
+        std::fill(domain.begin(), domain.begin() + DOMAIN_WIDTH, HOT);
+        std::fill(domain.begin() + (DOMAIN_HEIGHT - 1) * DOMAIN_WIDTH,
+                  domain.end(), HOT);
+
+        /// East & West: Cold
+        std::mdspan table{domain.data(), DOMAIN_WIDTH, DOMAIN_HEIGHT};
+        auto rows{std::views::iota(0u, DOMAIN_HEIGHT)};
+        for (auto y : rows) {
+            table[0, y] = HOT;
+            table[DOMAIN_WIDTH - 1, y] = HOT;
+        }
+    }};
+    set_boundary_conditions(domain_curr);
+    set_boundary_conditions(domain_next);
+
+    auto iterations{0u};
+    do {
+        std::mdspan table_curr{domain_curr.data(), DOMAIN_WIDTH, DOMAIN_HEIGHT};
+        std::mdspan table_next{domain_next.data(), DOMAIN_WIDTH, DOMAIN_HEIGHT};
+
+        auto inner_coords{std::views::cartesian_product(
+            std::views::iota(1u, DOMAIN_WIDTH - 1u),
+            std::views::iota(1u, DOMAIN_HEIGHT - 1u))};
+
+        norm_diff = 0.f;
+        for (auto [x, y] : inner_coords) {
+            auto stencil_sum{table_curr[x, y] + table_curr[x + 1, y] +
+                             table_curr[x - 1, y] + table_curr[x, y + 1] +
+                             table_curr[x, y - 1]};
+            table_next[x, y] = stencil_sum / 5.f;
+            auto diff{table_next[x, y] - table_curr[x, y]};
+            norm_diff += diff * diff;
+            std::println("diff @ {},{} = {}", x, y, diff);
+        }
+
+        std::println("nd @ iter {} = {}", iterations, norm_diff);
+        std::swap(domain_curr, domain_next);
+    } while (norm_diff > EPSILON && iterations++ < MAX_ITERATIONS);
+
     return 0;
 }

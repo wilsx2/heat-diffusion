abf3990301cf4d87533a8129bcf64983dbfd8896
William Conner <redwils5@gmail.com>
2026-08-05 13:31:22 -0400

    Heat map output to ppm

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
+#include <cstdlib>
+#include <fstream>
 #include <mdspan>
 #include <print>
 #include <ranges>
 #include <vector>
 
-constexpr auto DOMAIN_WIDTH{32u};
-constexpr auto DOMAIN_HEIGHT{32u};
-constexpr auto EPSILON{1.f};
+constexpr auto DOMAIN_WIDTH{1024u};
+constexpr auto DOMAIN_HEIGHT{1024u};
+constexpr auto EPSILON{1'000.f};
 constexpr auto MAX_ITERATIONS{100'000u};
 
 constexpr auto HOT{100.f};
 constexpr auto COLD{0.f};
 int main(int argc, char *argv[]) {
     // Init data
-    std::vector<float> domain_curr(DOMAIN_WIDTH * DOMAIN_HEIGHT, COLD);
+    std::vector<float> domain_curr(DOMAIN_WIDTH * DOMAIN_HEIGHT, (HOT + COLD) / 2.f);
     std::vector<float> domain_next(DOMAIN_WIDTH * DOMAIN_HEIGHT);
     auto norm_diff{0.f};
 
@@ -51,12 +53,26 @@ int main(int argc, char *argv[]) {
             table_next[x, y] = stencil_sum / 5.f;
             auto diff{table_next[x, y] - table_curr[x, y]};
             norm_diff += diff * diff;
-            std::println("diff @ {},{} = {}", x, y, diff);
         }
 
         std::println("nd @ iter {} = {}", iterations, norm_diff);
         std::swap(domain_curr, domain_next);
     } while (norm_diff > EPSILON && iterations++ < MAX_ITERATIONS);
+
+    // Save PPM
+    std::ofstream file("heatmap.ppm");
+    if (!file.is_open()) {
+        std::exit(EXIT_FAILURE);
+    }
+
+    file << "P3\n" << DOMAIN_WIDTH << " " << DOMAIN_HEIGHT << "\n" << 100 << "\n";
+    std::mdspan table{domain_curr.data(), DOMAIN_WIDTH, DOMAIN_HEIGHT};
+    for (auto x : std::views::iota(0u, DOMAIN_WIDTH)) {
+        for (auto y : std::views::iota(0u, DOMAIN_HEIGHT)) {
+            file << static_cast<unsigned>(table[x, y]) << " " << 0u << " " << static_cast<unsigned>(100 - table[x,y]) << " ";
+        }
+        file << "\n";
+    }
 
     return 0;
 }
